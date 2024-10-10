e082487a2187ba7b51b63031589cc5fb448f67bc
daeundan <ekdms534@naver.com>
2024-10-10 16:53:05 +0900

    241010 MusinsaShoppingMall first commit

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { Button, Container, Row, Col, Dropdown } from "react-bootstrap";
+
+const Img = styled.img`
+  width: 100%;
+  border-radius: 10px;
+`;
+
+const ProductDesc = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  font-size: 22px;
+  padding: 10px;
+`;
+
+const ProductTitle = styled.div`
+  font-weight: 600;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 18px;
+`;
+
+const ProductChoice = styled.div``;
+
+const ProductDetail = () => {
+  const [product, setProduct] = useState(null);
+  const { id } = useParams();
+  const getProductDetail = async () => {
+    const url = `http://localhost:3000/products/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setProduct(data);
+  };
+
+  const formattedPrice = new Intl.NumberFormat("ko-KR", {
+    style: "currency",
+    currency: "KRW",
+  }).format(product?.price);
+
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Img src={product && product?.img} alt={product && product?.id} />
+        </Col>
+        <Col>
+          <ProductDesc>
+            <Dropdown>
+              <ProductTitle>상품명 : {product && product?.title}</ProductTitle>
+              <ProductPrice>
+                상품가격 : {product && formattedPrice}
+              </ProductPrice>
+              <ProductChoice>
+                {product && product?.choice ? "Conscious choice" : ""}
+              </ProductChoice>
+            </Dropdown>
+            <Button variant="danger"></Button>
+            <Button variant="dark"></Button>
+          </ProductDesc>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ProductDetail;
